5351bf9abf9c1e7be2dd1d8b0ce2ab13ff5a62ac
amirhosseinh77 <amirhossein261077@live.com>
2021-01-01 00:30:04 +0330

    MPC is gold now!!!

--- control.py
+++ control.py
@@ -1,54 +1,110 @@
 import numpy as np
+from scipy.optimize import minimize
+
 
 class Car_Dynamics:
-    def __init__(self, x_0, y_0, v_0, phi_0, sample_time, length):
-        self.dt  = sample_time    # sampling time
-        self.L   = length          # wehicle length
+    def __init__(self, x_0, y_0, v_0, phi_0, length, dt):
+        self.dt = dt             # sampling time
+        self.L = length          # wehicle length
         self.x = x_0
         self.y = y_0
         self.v = v_0
         self.phi = np.deg2rad(phi_0)
-        self.u_k = np.array([[0, 0]]).T
-        self.z_k = np.array([[self.x, self.y, self.v, self.phi]]).T
+        self.state = np.array([[self.x, self.y, self.v, self.phi]]).T
 
-    def make_model(self, v, phi, delta):        
-        # matrices
-        # 4*4
-        A = np.array([[1, 0, self.dt*np.cos(phi)         , -self.dt*v*np.sin(phi)],
-                      [0, 1, self.dt*np.sin(phi)         , self.dt*v*np.cos(phi) ],
-                      [0, 0, 1                           , 0                     ],
-                      [0, 0, self.dt*np.tan(delta)/self.L, 1                     ]])
-        # 4*2 
-        B = np.array([[0      , 0                                  ],
-                      [0      , 0                                  ],
-                      [self.dt, 0                                  ],
-                      [0      , self.dt*v/(self.L*np.cos(delta)**2)]])
-
-        # 4*1
-        C = np.array([[self.dt*v* np.sin(phi)*phi                ],
-                      [-self.dt*v*np.cos(phi)*phi                ],
-                      [0                                         ],
-                      [-self.dt*v*delta/(self.L*np.cos(delta)**2)]])
-        
-        return A, B, C
+        # self.u_k = np.array([[0, 0]]).T
 
     def move(self, accelerate, steer):
         delta = np.deg2rad(steer)
-        u_k = np.array([[accelerate, delta]]).T
-        A,B,C = self.make_model(self.v, self.phi, delta)
-        z_k1 = A@self.z_k + B@u_k + C
-        return u_k, z_k1
+        x_dot = self.v*np.cos(self.phi)
+        y_dot = self.v*np.sin(self.phi)
+        v_dot = accelerate
+        phi_dot = self.v*np.tan(delta)/self.L
+        return np.array([[x_dot, y_dot, v_dot, phi_dot]]).T
 
 
-    def update_state(self, command, state):
-        self.u_k = command
-        self.z_k = state
-        self.x = self.z_k[0,0]
-        self.y = self.z_k[1,0]
-        self.v = self.z_k[2,0]
-        self.phi = self.z_k[3,0]
+    def update_state(self, state_dot):
+        # self.u_k = command
+        # self.z_k = state
+        self.state = self.state + self.dt*state_dot
+        self.x = self.state[0,0]
+        self.y = self.state[1,0]
+        self.v = self.state[2,0]
+        self.phi = self.state[3,0]
+
+    
+class MPC_Controller:
+    def __init__(self, horiz):
+        self.horiz = horiz
+        self.R = np.diag([0.01, 0.01])  # input cost matrix
+        self.Rd = np.diag([0.01, 1.0])  # input difference cost matrix
+        self.Q = np.diag([1.0, 1.0, 0.5, 0.5])  # state cost matrix
+        self.Qf = self.Q  # state final matrix
+
+    def mpc_cost(self, u_k, my_car, x_des, y_des, v_des, phi_des):
+        dt = my_car.dt            # sampling time
+        L = my_car.L            # wehicle length
+        x = my_car.x  
+        y = my_car.y  
+        v = my_car.v  
+        phi = my_car.phi 
+        u_k = u_k.reshape(self.horiz,2).T
+        z_k = np.zeros((4,self.horiz+1))
+        cost = 0.0
+        desired_state = np.array([x_des,y_des,v_des,phi_des])
+
+        for i in range(self.horiz):
+            x_dot = v*np.cos(phi)
+            y_dot = v*np.sin(phi)
+            v_dot = u_k[0,i]
+            phi_dot = v*np.tan(u_k[1,i])/L
+            
+            x += dt*x_dot
+            y += dt*y_dot
+            v += dt*v_dot
+            phi += dt*phi_dot
+            
+            z_k[:,i] = [x,y,v,phi]
+            cost += np.sum(self.R@(u_k[:,i]**2))
+            cost += np.sum(self.Q@((desired_state-z_k[:,i])**2))
+            # if i < (horiz-1):     
+            #     cost += np.sum(Rd@((u_k[:,i+1] - u_k[:,i])**2))
+
+        return cost
+
+    def optimize(self, my_car, x_des, y_des, v_des, phi_des):
+        result = minimize(self.mpc_cost, args=(my_car, x_des, y_des, v_des, phi_des), x0 = np.zeros((2*self.horiz)), method='BFGS')
+        return result.x[0],  result.x[1]
+
+
+
+    # def make_model(self, v, phi, delta):        
+    #     # matrices
+    #     # 4*4
+    #     A = np.array([[1, 0, self.dt*np.cos(phi)         , -self.dt*v*np.sin(phi)],
+    #                   [0, 1, self.dt*np.sin(phi)         , self.dt*v*np.cos(phi) ],
+    #                   [0, 0, 1                           , 0                     ],
+    #                   [0, 0, self.dt*np.tan(delta)/self.L, 1                     ]])
+    #     # 4*2 
+    #     B = np.array([[0      , 0                                  ],
+    #                   [0      , 0                                  ],
+    #                   [self.dt, 0                                  ],
+    #                   [0      , self.dt*v/(self.L*np.cos(delta)**2)]])
+
+    #     # 4*1
+    #     C = np.array([[self.dt*v* np.sin(phi)*phi                ],
+    #                   [-self.dt*v*np.cos(phi)*phi                ],
+    #                   [0                                         ],
+    #                   [-self.dt*v*delta/(self.L*np.cos(delta)**2)]])
         
+    #     return A, B, C
 
+    # def move(self, accelerate, steer):
+    #     delta = np.deg2rad(steer)
+    #     u_k = np.array([[accelerate, delta]]).T
+    #     A,B,C = self.make_model(self.v, self.phi, delta)
+    #     z_k1 = A@self.z_k + B@u_k + C
+    #     return u_k, z_k1
         
 # class MPC_Controller:
 #     def __init__():
--- main_autopark.py
+++ main_autopark.py
@@ -6,12 +6,19 @@ import matplotlib.pyplot as plt
 
 from environment import Environment
 from pathplanning import PathPlanning
+from control import *
+from scipy.optimize import minimize
+
+my_car = Car_Dynamics(0,0,0,45,4,0.2)
+controller = MPC_Controller(horiz=5)
 
 # environment margin  : 3
 # pathplanning margin : 2
 
 # obs = np.array([[30,i] for i in range(-3,80)] + [[70,i] for i in range(20,103)]) 
 obs = np.array([[30,i] for i in range(30,70)] + [[70,i] for i in range(30,70)] + [[i,30] for i in range(30,70)] + [[i,70] for i in range(30,70)]) 
+# new_obs = np.array([[78,78],[79,79],[78,79]])
+# obs = np.vstack([obs,new_obs])
 
 env = Environment(obs)
 car_center = np.array([0,0])
@@ -27,9 +34,16 @@ print('path = \n',interpolated_path)
 
 env.draw_path(interpolated_path)
 
-while True:
+# interpolated_path[interpolated_path<0]=0
+
+for i,point in enumerate(interpolated_path[::-1][10:]):
+        computed_angle = np.arctan((interpolated_path[::-1][10:][i+5][1]-interpolated_path[::-1][10:][i][1])/(interpolated_path[::-1][10:][i+5][0]-interpolated_path[::-1][10:][i][0]))
+        print(point)
+
+        acc, delta = controller.optimize(my_car,point[0],point[1],1,computed_angle)
+        my_car.update_state(my_car.move(acc,  np.rad2deg(delta)))
         
-        res = env.render(car_center[0],car_center[1],ang)
+        res = env.render(int(my_car.x*10),int(my_car.y*10),int(np.rad2deg(my_car.phi)))
         # cv2.imwrite('res.png', res*255)
         # break
         cv2.imshow('environment', res)
